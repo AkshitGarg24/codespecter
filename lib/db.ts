de45c61943c72fabc126d8b814c7ffe2561e57b2
Akshit Garg <akshitgarg2412@gmail.com>
2025-12-15 23:02:04 +0530

    prettier and eslint configuration

--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,20 @@
-import { PrismaClient } from "./generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from './generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL
-})
+  connectionString: process.env.DATABASE_URL,
+});
 
 const prismeClientSingleton = () => {
-    return new PrismaClient({ adapter    })
-}
+  return new PrismaClient({ adapter });
+};
 
 declare const globalThis: {
-    prismaGlobal: ReturnType<typeof prismeClientSingleton>;
+  prismaGlobal: ReturnType<typeof prismeClientSingleton>;
 } & typeof global;
 
 const prisma = globalThis.prismaGlobal || prismeClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
 
 export default prisma;
